1517ec0fe5682605986c79245e1c199aa65c5bee
Dietrich <ldietrichl@ya.ru>
2024-06-22 23:06:05 +0300

    Выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -0,0 +1,20 @@
+package ru.stqa.geometry;
+
+import ru.stqa.geometry.figures.Rectangle;
+import ru.stqa.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+
+        Square.printSquareArea(7.);
+        Square.printSquareArea(5.);
+        Square.printSquareArea(3.);
+
+        Rectangle.printRectangleArea(3.0, 5.0);
+        Rectangle.printRectangleArea(7.0,9.0);
+        
+
+    }
+
+
+}
