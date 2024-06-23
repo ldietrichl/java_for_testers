30a7a484bf8429f73746a165ea83c29d7ec4c7a2
Dietrich <ldietrichl@ya.ru>
2024-06-23 20:50:41 +0300

    Лекция 2, интервальный коммит

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,6 +2,12 @@ package ru.stqa.geometry.figures;
 
 public record Rectangle(double a, double b) {
 
+    public Rectangle{
+        if(a<0||b<0){
+            throw  new IllegalArgumentException("Rectangle side should be non-negative");
+        }
+    }
+
 
 
     public static void printRectangleArea(double a, double b) {
